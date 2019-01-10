8d6ea4d6b7a725314398e059b842ce66a2437632
Agaba-Milton <43774933+Agaba-Milton@users.noreply.github.com>
2019-01-10 17:25:53 +0300

    Add files via upload

---
+++ python_convo/convo.py
@@ -0,0 +1,55 @@
+
+name = input('Hi what is your name....?  :')
+print('hello',' '+ name)
+
+gender = input('Are you male or female..?   :')
+print('Ohh...you are a',' '+ gender)
+
+age = input('How old are you..?  :')
+print(name,'are you really', age)
+
+truth = input('Thanks for telling the truth, Hope youre doing fine  :')
+print('If ', ' '+truth, 'then thats awesome......Lets continue')
+
+address = input('Okay.. fine....Where do you live..?  :')
+print('I just had a feeling you live in ',' '+ address)
+
+occupation = input('So where do you work buddy :')
+print('Ohh...You are currently working with' ' '+occupation)
+
+position = input('What is your position..Im getting interested..?  :')
+print('Indeed you look to be',' '+ position)
+
+job = input('Actually is there any free vacancy for a man like me..?  :')
+print('I will be grateful if theres any..hope to get positive feedback buddy')
+
+
+
+salary = input('Then buddy keep the integrity....how much do you earn..? :')
+print('Wow you are getting',' '+ salary, 'as your salary')
+
+color = input('Lets leave that behind.....buddy whats your favorite color')
+print('Oh My God..',' '+ color, 'is also my favorite color')
+
+food = input('Whats your favorite dish buddy..?:')
+print('Sorry buddy for me im not a fan of',' '+ food)
+
+status = input('Buddy be honest......What is your status :')
+print('I just cant imagine youre',' '+ status)
+
+rate = input('Buddy its time to rate my service....Honestly how many stars do i deserve..? :')
+print('Thank you for making me special by giving me',' '+ rate, 'stars')
+
+appreciate = input('Thank you for your good time buddy...hope talking to you again :')
+print('If......',' '+ appreciate, 'That means i havent been a bore to you')
+
+remark = input('Before leaving dont forget to always come back and visit Kampabits :')
+print('If',' '+ remark, 'You will be warmly welcomed and doors will be open for you ')
+       
+bye = input('I dont wanna say goodbye, but i have no choice buddy...BYEE....BYEEE :')
+print('if...',' '+ bye, 'hope to hear from you again....Nice time...Aurevoir....Cia..Cia....Webale kujja :')
+
+
+
+
+
